45bc350fa4e979efcf1bf0d25dbb4872c5ffc001
Christian Sveen <casveen@student.matnat.uio.no>
2020-02-24 15:32:06 +0100

    Update numberTheory.hs

--- numberTheory.hs
+++ numberTheory.hs
@@ -13,6 +13,32 @@ primesUpTo n
            | n<=0      = error "cannot give list of primes up to non-positive value"
            | otherwise = filter isPrime [2..n]
 
+--jacobiSymbol :: Int -> Int -> Int
+--jacobiSymbol a n
+--    |
+--    |
+--    |
+
+dotProduct2 :: (Num a) => (a,a) -> (a,a) -> a
+dotProduct2 (a1, a2) (a3, a4) = a1*a3+a2*a4
+
+chineseRemainder :: [(Int, Int)] -> (Int, Int)
+-- example x === 3 mod 5
+--         x === 2 mod 9   =>  chineseRemainder [(3,5), (2,9), (7,23)] = ()
+--         x === 7 mod 23
+chineseRemainder [] = error "empty input to chineseRemainder"
+chineseRemainder ((a1, n1):[]) = (a1, n1)
+chineseRemainder ((a1, n1):(a2, n2):rest)
+    | g>1 &&  ((mod a1 g) == (mod a2 g)) = chineseRemainder ((a3 `div` g, n3):rest)
+    | g>1                               = (negate 1, negate 1)
+    | otherwise                         = chineseRemainder ((a3, n3):rest) -- coprime
+    where g   = gcd n1 n2
+          n1' = modInv n1 n2
+          n3  =n1*n2
+          a3  = mod (a2*(n1'*n1)+ a1*(1-n1'*n1)) n3 --dotProduct2 (head (gcdValues n1 n2)) (a1, a2)
+
+
+
 gcd' :: Int -> Int -> Int
 -- finding the gcd amounts to running the euclidean algorithm,
 -- which is based on the residuals r_(k-2)=q_k r_(k-1)+r_k
@@ -52,6 +78,17 @@ modInv a n
                  iterator  = \(q,r) (acc_a, acc_b) -> (acc_b, acc_a -q*acc_b+n) --- +n to get in 0< <n value
                  --foldStart = (0::Int,1::Int)
 -- [ a*(a `modInv` n) `mod` n | n<-primesUpTo 1000, a<-[1..n-1]] is all ones, beibi!
+
+modInvBoth :: Int -> Int -> (Int, Int)
+modInvBoth a n
+           | a>n       = modInvBoth (a `mod` n) n
+           | a<0       = modInvBoth (n - ((negate a) `mod` n)) n
+           | otherwise = foldr1 iterator qr
+           where qr        = gcdValues a n
+                 iterator  = \(q,r) (acc_a, acc_b) -> (acc_b, acc_a -q*acc_b+n) --- +n to get in 0< <n value
+                 --foldStart = (0::Int,1::Int)
+-- [ a*(a `modInv` n) `mod` n | n<-primesUpTo 1000, a<-[1..n-1]] is all ones, beibi!
+
 
 toBinary :: Int -> [Int]
 toBinary 1 = [1]
